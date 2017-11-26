00d7f491c7bb6774a7b97329e2dd8c05ce180044
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-26 04:11:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
-$(document).ready(function(){
+$(document).ready(function(){});
 
-// call functions here
+function getIt(){
+  $('p').on('click', function(){
+    
+  alert("Hey!")
+  })
+}
 
-});
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+  }
+  
+function pressIt(){
+  $("#typing").on('keydown', function(key){
+    if(key.which == 71){
+    alert("Your form is going to be submitted now.")
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+
